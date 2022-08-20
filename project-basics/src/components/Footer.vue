6ec228d5e8e547e6aec4f0ab48a43d24a35b9511
David Leon <97davidleon@gmail.com>
2022-08-20 19:15:34 +0200

    Forgot to create develop branch, iteration 3 done

--- project-basics/src/components/Footer.vue
+++ project-basics/src/components/Footer.vue
@@ -1,19 +1,22 @@
 <template>
-<div>
-    <h1>Esto es un footer</h1>
-    <h3>Calculadora: {{ 3 + 3 }}</h3>
-    <h2>Prop from App.vue: {{ myNumber }}</h2>
-    <h3>Función: {{ returnString() }} </h3>
-    <h3>Atributo: {{ project }}</h3>
-    <h1 v-if="2>1">Probando v-if</h1>
-    <h1 v-else>Oh nooooo</h1>
-</div>
+    <div>
+        <h1>Esto es un footer</h1>
+        <h3>Calculadora: {{ 3 + 3 }}</h3>
+        <h2>Prop from App.vue: {{ myNumber }}</h2>
+        <h3>Función: {{ returnString() }} </h3>
+        <h3>Atributo: {{ project }}</h3>
+        <h3 v-if="varIt3 > 2">Iteration 3: Probando v-if</h3>
+        <h3 v-else-if="varIt3 == 2">Iteration3: Probando v-else-if</h3>
+        <h3 v-else>Iteration 3: Probando v-else</h3>
+        <h3>Probando condicional ternario: {{ varIt3 === 2 ? project : returnString() }}</h3>
+        <h3 v-show="varIt3 === 2">Probando v-show! Está en el div pero cambia la propiedad de visibilidad</h3>
+    </div>
 </template>
 
 <script>
 
 export default {
-    name:"Footer",
+    name: "Footer",
     props: {
         myNumber: Number,
     },
@@ -21,7 +24,8 @@ export default {
     data() {
         return {
             numb: 8,
-            project: " ",
+            project: "Inicialmente es asi",
+            varIt3: 3,
         }
     },
 
